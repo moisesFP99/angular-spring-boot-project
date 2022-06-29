3f1f1f101de9de25abe4283a16064c7eaa9f5a35
Moises <moisesxd8@gmail.com>
2022-06-29 17:13:34 -0300

    todos login welcome and components

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  username = 'moisesxd8'
+  password = ''
+  errorMessage = 'Invalid Credentials'
+  invalidLogin = false
+
+  //Router
+  //Angular.giveMeRouter
+  //Dependency Injection
+  constructor(private router: Router) { }
 
   ngOnInit() {
   }
 
+  handleLogin() {
+    // console.log(this.username);
+    if(this.username==="moisesxd8" && this.password === '123456') {
+      //Redirect to Welcome Page
+      this.router.navigate(['welcome', this.username])
+      this.invalidLogin = false
+    } else {
+      this.invalidLogin = true
+    }
+  }
 }
