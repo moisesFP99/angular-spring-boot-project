84d0929ebcdbe2a63df71079d30414e7d93319c9
Moises <moisesxd8@gmail.com>
2022-06-30 00:43:52 -0300

    add new components

--- src/app/list-todos/list-todos.component.ts
+++ src/app/list-todos/list-todos.component.ts
@@ -1,5 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
+export class Todo {
+  constructor(
+    public id: number,
+    public description: string,
+    public done: boolean,
+    public targetDate: Date
+  ){
+
+  }
+}
+
 @Component({
   selector: 'app-list-todos',
   templateUrl: './list-todos.component.html',
@@ -8,9 +19,12 @@ import { Component, OnInit } from '@angular/core';
 export class ListTodosComponent implements OnInit {
 
   todos = [
-    {id : 1, description : 'Learn to Dance'},
-    {id : 2, description : 'Become an Expert at Angular'},
-    {id : 3, description : 'Visit Norway'}
+    new Todo(1, 'Learn to Dance', false, new Date()),
+    new Todo(2, 'Become an Expert at Angular', false, new Date()),
+    new Todo(3, 'Visit Norway', false, new Date())
+    // {id : 1, description : ''},
+    // {id : 2, description : ''},
+    // {id : 3, description : ''}
   ]
 
   // todo = {
